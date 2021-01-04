d79fe2870708fb8efc044c21f1501660cd87e667
LeahESc <leahemily1@gmail.com>
2021-01-04 09:09:17 -0800

    adds seed data

--- trip-itinerary-maker-backend/db/seeds.rb
+++ trip-itinerary-maker-backend/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: "Nightlife")
+Category.create(name: "Restaurants")
+Category.create(name: "Museums")
+Category.create(name: "Hikes")
+Category.create(name: "Tours")
+Category.create(name: "Sights")
